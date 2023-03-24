7694d6865903969798aedf8806d1752782cf2c70
lovecodingmonkey <2698701835@qq.com>
2023-03-24 19:02:39 +0800

    v 1.0

---
+++ src/main/java/com/zgy/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.zgy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("hello2")
+
+public class HelloController {
+
+       @RequestMapping("")
+
+       public String hello() {
+
+              return "helloworld2";
+
+       }
+
+}
